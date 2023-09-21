7570e6ae7ce21fa885a20341ac5aab04b1048e26
edaurdo1234 <saesilva.e@gmail.com>
2023-09-21 18:18:07 +0200

    fix remember me checkbox

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,26 +10,23 @@ use Symfony\Component\Mercure\Update;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
-    //push
-    #[Route('/publish', name: 'app_publish')]
+
     public function publish(HubInterface $hub): Response
     {
         $update = new Update(
-            'https://localhost/books/1',
-            json_encode(['status' => 'message recu'])
+            'https://example.com/books/1',
+            json_encode(['status' => 'OutOfStock'])
         );
 
         $hub->publish($update);
 
         return new Response('published!');
     }
-    //souscrire au push
-
 }
